e2eb713aa6304aa7801eeb2da7a617d756334c7c
Joris Roovers <joris.roovers@gmail.com>
2014-07-21 13:18:59 +0200

    Added some packages
    - Added virtualenv and pip
    - Foundation for automatically downloading pycharm (non functional)

--- misc/install.sh
+++ misc/install.sh
@@ -8,7 +8,18 @@ config_gedit(){
    gsettings set org.gnome.gedit.preferences.editor insert-spaces true
 }
 
+download_pycharm(){
+    wget -O ~/Downloads/pycharm-community-3.4.1.tar.gz \ 
+        http://download.jetbrains.com/python/pycharm-community-3.4.1.tar.gz
+}
+
+
+
 echo "Configuring gedit.."
 config_gedit
 echo "DONE"
-
+# There are some measures in place that prevent us to download pycharm
+# automatically, so we don't do it for now.
+#echo "Downloading pycharm"
+#download_pycharm
+#echo "DONE"
